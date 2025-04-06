184b50bb330446ceb36f7bf476f37581111a4be8
wkp48 <61016821+wkp48@users.noreply.github.com>
2025-04-06 13:53:21 +0900

    Add files via upload

--- src/Members/term.js
+++ src/Members/term.js
@@ -8,6 +8,11 @@ import 김재영 from '../assets/김재영.jpeg'
 import 김신의 from '../assets/김신의.jpeg'
 import 김은성 from '../assets/김은성.jpeg'
 import 김채연 from '../assets/김채연.jpeg'
+import 전진호 from '../assets/전진호.jpeg'
+import 홍지우 from '../assets/홍지우.jpeg'
+import 기본이미지 from '../assets/default-profile.png'
+
+
 function Term() {
   const members = [
     {
@@ -44,19 +49,19 @@ function Term() {
       name: '김가빈',
       position: 'Frontend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '김민오',
       position: 'Frontend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '김주현',
       position: 'Frontend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '김채연',
@@ -68,98 +73,98 @@ function Term() {
       name: '김태현',
       position: 'Frontend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '박세령',
       position: 'Frontend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '서우진',
       position: 'Frontend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '전은영',
       position: 'Frontend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '한지성',
       position: 'Frontend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
 
     {
       name: '고재휘',
       position: 'Backend',
       department: '인공지능소프트웨어학과',
-      image: '홍지우_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '김현우',
       position: 'Backend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '박지우',
       position: 'Backend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '신숙우',
       position: 'Backend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '이유진',
       position: 'Backend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '전진호',
       position: 'Backend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 전진호
     },
     {
       name: '전준범',
       position: 'Backend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '정승준',
       position: 'Backend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '조현수',
       position: 'Backend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '홍성진',
       position: 'Backend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 기본이미지
     },
     {
       name: '홍지우',
       position: 'Backend',
       department: '인공지능소프트웨어학과',
-      image: '박정민_이미지_경로'
+      image: 홍지우
     }
   ];
 
